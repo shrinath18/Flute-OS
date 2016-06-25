24b13fce440e23568d777a0ca8d79ec7deed3629
Shrinath Kalamdani <shrinath.kalamdani@flipkart.com>
2016-06-25 15:36:43 +0530

    Hello World kernel

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 
 int main(struct multiboot *mboot_ptr)
 {
+    
+    print("Hello World");
     return 0xDEADBABA;
+}
+
+void print(char * message){
+    unsigned short * where_to_print = (unsigned short *)0xB8000 + 323;//VGA memory starts at 0xB8000 
+    unsigned short attribute = 14;
+    unsigned short character = message[0];
+    int i = 1;
+    while(character != '\0'){
+     *where_to_print = character | (attribute << 8);
+     character = message[i];
+     i++;
+     where_to_print += 1;
+    }
+
 }
